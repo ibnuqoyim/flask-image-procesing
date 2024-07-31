e9591bd627e68d21b14ac57bfe53764e78803fe2
Muhamad Ibnu Qoyim <muhamadibnu9@gmail.com>
2024-07-31 22:08:29 +0700

    [feature] add feature to processing image

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,13 @@
-import os
 from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
-from .utils import process_image
+import os
+from .utils import convert_to_grayscale, resize_image, rotate_image, edge_detection, blur_image, adjust_brightness_contrast
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
     return render_template('index.html')
-
 
 @bp.route('/upload', methods=['POST'])
 def upload_file():
@@ -28,7 +27,32 @@ def upload_file():
 def show_image(filename):
     return render_template('result.html', filename=filename)
 
-@bp.route('/process/<filename>')
-def process_image_route(filename):
-    output_filename = process_image(filename)
-    return send_from_directory('instance/uploads', output_filename)
+@bp.route('/process/grayscale/<filename>')
+def process_grayscale(filename):
+    output_filename = convert_to_grayscale(os.path.join('instance/uploads', filename))
+    return send_from_directory('instance/uploads', os.path.basename(output_filename))
+
+@bp.route('/process/resize/<filename>')
+def process_resize(filename):
+    output_filename = resize_image(os.path.join('instance/uploads', filename), 200, 200)
+    return send_from_directory('instance/uploads', os.path.basename(output_filename))
+
+@bp.route('/process/rotate/<filename>')
+def process_rotate(filename):
+    output_filename = rotate_image(os.path.join('instance/uploads', filename), 45)
+    return send_from_directory('instance/uploads', os.path.basename(output_filename))
+
+@bp.route('/process/edges/<filename>')
+def process_edges(filename):
+    output_filename = edge_detection(os.path.join('instance/uploads', filename), 100, 200)
+    return send_from_directory('instance/uploads', os.path.basename(output_filename))
+
+@bp.route('/process/blur/<filename>')
+def process_blur(filename):
+    output_filename = blur_image(os.path.join('instance/uploads', filename), 5)
+    return send_from_directory('instance/uploads', os.path.basename(output_filename))
+
+@bp.route('/process/adjust/<filename>')
+def process_adjust(filename):
+    output_filename = adjust_brightness_contrast(os.path.join('instance/uploads', filename), 1.2, 50)
+    return send_from_directory('instance/uploads', os.path.basename(output_filename))
--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,48 @@
 import os
 from PIL import Image
+import cv2
 
-def process_image(filename):
-    img_path = os.path.join('instance/uploads', filename)
-    img = Image.open(img_path)
-    img = img.convert('L')
-    output_filename = 'processed_' + filename
-    img.save(os.path.join('instance/uploads', output_filename))
-    return output_filename
+def convert_to_grayscale(image_path):
+    img = cv2.imread(image_path)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    output_path = 'instance/uploads/gray_' + os.path.basename(image_path)
+    cv2.imwrite(output_path, gray_img)
+    return output_path
+
+def resize_image(image_path, width, height):
+    img = cv2.imread(image_path)
+    resized_img = cv2.resize(img, (width, height))
+    output_path = 'instance/uploads/resized_' + os.path.basename(image_path)
+    cv2.imwrite(output_path, resized_img)
+    return output_path
+
+def rotate_image(image_path, angle):
+    img = cv2.imread(image_path)
+    (h, w) = img.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated_img = cv2.warpAffine(img, M, (w, h))
+    output_path = 'instance/uploads/rotated_' + os.path.basename(image_path)
+    cv2.imwrite(output_path, rotated_img)
+    return output_path
+
+def edge_detection(image_path, lower_threshold, upper_threshold):
+    img = cv2.imread(image_path)
+    edges = cv2.Canny(img, lower_threshold, upper_threshold)
+    output_path = 'instance/uploads/edges_' + os.path.basename(image_path)
+    cv2.imwrite(output_path, edges)
+    return output_path
+
+def blur_image(image_path, ksize):
+    img = cv2.imread(image_path)
+    blurred_img = cv2.GaussianBlur(img, (ksize, ksize), 0)
+    output_path = 'instance/uploads/blurred_' + os.path.basename(image_path)
+    cv2.imwrite(output_path, blurred_img)
+    return output_path
+
+def adjust_brightness_contrast(image_path, alpha, beta):
+    img = cv2.imread(image_path)
+    adjusted_img = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
+    output_path = 'instance/uploads/adjusted_' + os.path.basename(image_path)
+    cv2.imwrite(output_path, adjusted_img)
+    return output_path
